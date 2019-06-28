fd5bd20e47ad39a5e380223f2f84da668a8bd09e
JosueIsmaelLopez <36417568+JosueIsmaelLopez@users.noreply.github.com>
2019-06-27 22:33:18 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ReservaComponent } from './components/reserva/reserva.component';
+import { CardComponent } from './components/card/card.component';
+
+//Definiendo un Objeto de tipo Routes con todas las rutas que manejara la aplicacion
+const routes: Routes = [
+  {path:'', component: ReservaComponent, pathMatch: 'full'},
+  // {path:'reserva', component: ReservaComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    ReservaComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, //Se importa para el soporte de formularios de angular
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
