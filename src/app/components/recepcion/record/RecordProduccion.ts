40632de9f9fe61de39c1984be6b22f2f9b9f6326
JosueIsmaelLopez <36417568+JosueIsmaelLopez@users.noreply.github.com>
2019-07-06 01:39:13 -0600

    Cambiando las dimensiones de rejillas

---
+++ src/app/components/recepcion/record/RecordProduccion.ts
@@ -0,0 +1,13 @@
+export class RecordProduccion {
+  no: Number;
+  telefono: string;
+  NoMat: Number;
+  inscripcion: string;
+  plan: Number;
+  valorMes: Number;
+  valorTotal: Number;
+  ejecutivo: string;
+  estrategia: string;
+  formaPago: string;
+  observacion: string;
+}
--- src/app/components/recepcion/record/record.component.ts
+++ src/app/components/recepcion/record/record.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecordProduccion } from './RecordProduccion';
 
 @Component({
   selector: 'app-record',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordComponent implements OnInit {
 
+  record: RecordProduccion[];
   constructor() { }
 
   ngOnInit() {
